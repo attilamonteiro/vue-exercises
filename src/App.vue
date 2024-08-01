840531dfc02145293df86f35c3b7d15dc67782f9
Attila <attila.abreu@leste.com>
2024-08-01 16:52:52 -0300

    exercise defineModel

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ first }} {{ last }}</h1>
+  <UserName
+    :first-name="first"
+    :last-name="last"
+    @update:first-name="first = $event"
+    @update:last-name="last = $event"
+  >
+    <!-- Conteúdo adicional passado via slot -->
+    <p>Additional content here.</p>
+    <button @click="first = ''; last = ''">Clear</button>
+  </UserName>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref } from 'vue'
+import UserName from './components/UserName.vue'
+
+const first = ref('John')
+const last = ref('Doe')
+</script>
+
+
---
+++ src/components/UserName.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { toRefs, computed } from 'vue'
+
+const props = defineProps({
+  firstName: String,
+  lastName: String
+})
+
+const emit = defineEmits(['update:firstName', 'update:lastName'])
+
+const { firstName, lastName } = toRefs(props)
+
+const localFirstName = computed({
+  get: () => firstName.value,
+  set: value => emit('update:firstName', value)
+})
+
+const localLastName = computed({
+  get: () => lastName.value,
+  set: value => emit('update:lastName', value)
+})
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="localFirstName" placeholder="First Name" />
+    <input type="text" v-model="localLastName" placeholder="Last Name" />
+    <!-- Slot para conteúdo adicional -->
+    <slot></slot>
+  </div>
+</template>
